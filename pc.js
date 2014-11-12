ad6e5fb80d1d7126d06a5e56e41cb9acaaeb1e53
青柳　恭義 <uu070970@CA0250.local>
2014-11-12 19:03:04 +0900

    Refactoring: beautify code

--- pc.js
+++ pc.js
@@ -73,8 +73,84 @@ var app = app || {};
 	    $("#fee_tabs").tabs();
 	}
     });
+
+    app.GoogleMapView = Backbone.View.extend({
+
+	initialize: function (options) {
+	    this.addr = options.addr;
+	    this.el = options.el;
+	},
+
+	render: function () {
+	    var that = this,
+		geocoder = new google.maps.Geocoder();
+	    async.waterfall([
+		function (cb) {
+		    geocoder.geocode({
+			'address': that.addr,
+			'region': 'jp'
+		    }, function (results, status) {
+			var err;
+			if (status !== google.maps.GeocoderStatus.OK) {
+			    err = status;
+			}
+			cb(err, results);
+		    });
+		}, function (results, cb) {
+		    Object.keys(results).forEach(function (key) {
+			var result = results[key];
+			if (!result.geometry) {
+			    cb(null);
+			    return;
+			}
+			var latlng = result.geometry.location;
+			var bounds = new google.maps.LatLngBounds();
+			var opts = {
+			    zoom: 15,
+			    center: latlng,
+			    mapTypeId: google.maps.MapTypeId.ROADMAP,
+			    mapTypeControl: false
+			};
+			bounds.extend(latlng);
+			map = new google.maps.Map(document.getElementById(that.el), opts);
+			var marker = new google.maps.Marker({
+			    position: latlng,
+			    map: map
+			});
+		    });
+		}
+	    ], function (err) {
+		return that;
+	    });
+	}
+    });
     
     app.init = function () {
+
+	
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-25444755-1']);
+	_gaq.push(['_trackPageview']);
+
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+	// google maps
+        var m1 = new app.GoogleMapView({
+	    addr: '埼玉県蕨市中央4-21',
+	    el: "warabi_map"
+	});
+	m1.render();
+        var m2 = new app.GoogleMapView({
+	    addr: '埼玉県川口市並木２－８－２',
+	    el: "kawaguchi_map"
+	});
+	m2.render();
+	// Menu Tabs
+        $('#tabs').tabs();
+
+	// Free Tabs
 	new app.FeeView();
     };
 })();
